0500fd27b571c09d6930923771ea0e3aa77e3879
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-10-24 19:10:28 +0530

    push it

---
+++ next-sitemap.config.js
@@ -0,0 +1,24 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://youtubelive.vercel.app/', // Replace with your actual live domain
+  changefreq: 'daily',
+  sitemapSize: 7000,
+  // generateRobotsTxt: true, // Optionally generate robots.txt
+  transform: async (config, path) => {
+    if (path === '/') {
+      return {
+        loc: path, // The homepage
+        changefreq: config.changefreq,
+        priority: 1.0, // Highest priority for the homepage
+        lastmod: new Date().toISOString(),
+      };
+    }
+    return {
+      loc: path, // All other pages
+      changefreq: config.changefreq,
+      priority: 0.9, // Slightly lower priority for other pages
+      lastmod: new Date().toISOString(),
+    };
+  },
+};
